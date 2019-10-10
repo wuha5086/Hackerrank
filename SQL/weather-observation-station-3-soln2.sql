0b8689046f1824518c6147037fa5e77c10d9ca72
Han <31714576+wuha5086@users.noreply.github.com>
2019-10-09 23:04:20 -0400

    Revert "update"
    This reverts commit 9ee8aa2eb8ed962d758e7f8aa1485321b5ab002f.

--- SQL/weather-observation-station-3-soln2.sql
+++
@@ -1,5 +0,0 @@
-/*
-Enter your query here.
-*/
-SELECT DISTINCT CITY FROM STATION
-WHERE ID % 2 = 0;
--- SQL/weather-observation-station-3.sql
+++
@@ -1,7 +0,0 @@
-/*
-Enter your query here.
-*/
-SELECT DISTINCT CITY
-FROM STATION
-WHERE MOD(ID, 2) = 0
-ORDER BY CITY;
