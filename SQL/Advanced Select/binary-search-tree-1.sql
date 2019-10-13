a7c789e87607a99068be2684ba1d847921e2b8e4
Han <31714576+wuha5086@users.noreply.github.com>
2019-10-13 12:23:58 -0400

    Create binary-search-tree-1.sql

---
+++ SQL/Advanced Select/binary-search-tree-1.sql
@@ -0,0 +1,11 @@
+/*
+Enter your query here.
+*/
+SELECT N, 
+CASE 
+    WHEN P IS NULL THEN 'Root' 
+    WHEN(SELECT COUNT(*) FROM BST WHERE P = B.N) > 0 THEN 'Inner'
+    ELSE 'Leaf'
+END
+FROM BST AS B
+ORDER BY N;
